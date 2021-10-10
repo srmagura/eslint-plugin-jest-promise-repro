4e7e9553f63fe70881b3dafd6a7df783bf59b911
Sam Magura <srmagura@gmail.com>
2021-10-10 17:30:00 -0400

    simplify

--- index.test.js
+++ index.test.js
@@ -1,11 +1,8 @@
 it("handles an error", async () => {
-  jest.useFakeTimers();
-
   const p = Promise.reject(new Error("test error")).then(undefined, (e) => {
     expect(e).toBeInstanceOf(Error);
     return "handled";
   });
-  jest.runAllTimers();
 
   expect(await p).toBe("handled");
 });
